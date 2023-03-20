c0ca7a123499adf404f0e15f8c79e304d998c2f5
SaarChaffee <1240148332@qq.com>
2023-03-20 18:21:53 +0800

    feat: add ascii2d support

--- skadi/plugins/picsearch/__init__.py
+++ skadi/plugins/picsearch/__init__.py
@@ -1,10 +1,12 @@
+import asyncio
 import aiohttp
 from nonebot import on_command
 from nonebot.adapters.onebot.v11 import MessageEvent, Message
-from nonebot.log import logger
 from nonebot.rule import to_me
+from functools import partial
 from pydantic import parse_obj_as
 from bs4 import BeautifulSoup
+import cloudscraper
 
 from skadi import config
 from .types import *
@@ -15,48 +17,91 @@ resp = on_command("search", rule=to_me(), aliases={"搜图"}, priority=5)
 
 @resp.handle()
 async def get_pic(event: MessageEvent):
-    back: Message = Message()
-    back += Message.template("{str}").format(str="SauceNao")
-    
     for msg in event.reply.message["image"] if event.reply else event.message["image"]:
         pic_url = msg.data["url"]
-        _data = parse_obj_as(JsonData, await search_from_saucenao(pic_url)).results[0]
-        back += Message.template("{link:image}\n").format(link=_data.header.thumbnail)
-        for url in _data.data.dict()['ext_urls']:
-            back += Message.template("url : {url}\n").format(url=url)
-        back += Message.template("title : {title}\n").format(title=_data.data.dict()[data_models[_data.header.index_id]['title']])
-        back += Message.template("author : {author}\n").format(author=_data.data.dict()[data_models[_data.header.index_id]['author']])
-        
-    await resp.finish(back)
+        await send_data_of_saucenao(pic_url)
+
+
+async def send_data_of_saucenao(pic_url: str):
+    back: Message = Message()
+    back += Message.template("{str}").format(str="SauceNao")
+    _data = parse_obj_as(JsonData, await search_from_saucenao(pic_url)).results[0]
+    
+    back += Message.template("{link:image}\n").format(link=_data.header.thumbnail)
+    back += Message.template("title : {title}\n").format(title=_data.data.dict()[data_models[_data.header.index_id]['title']])
+    back += Message.template("author : {author}\n").format(author=_data.data.dict()[data_models[_data.header.index_id]['author']])
+    for url in _data.data.dict()['ext_urls']:
+        back += Message.template("url : {url}\n").format(url=url)
+    await resp.send(back)
+    if float(_data.header.similarity) < config.threshold:
+        await resp.send(Message.template("{str}").format(str="相似度过低，尝试使用ascii2d特征搜索"))
+        await send_data_of_ascii2d(pic_url)
+
+
+async def send_data_of_ascii2d(pic_url: str):
+    back: Message = Message()
+    back += Message.template("{str}").format(str="Ascii2d")
+    datas = await search_from_ascii2d(pic_url)
+    _data = datas[0]
+    back += Message.template("{link:image}\n").format(link=_data['thumbnail'])
+    for __data in _data['datas']:
+        back += Message.template("title : {title}\n").format(title=__data['title'])
+        back += Message.template("source : {source}\n").format(source=__data['source'])
+        back += Message.template("author : {author}\n").format(author=__data['author'])
+    await resp.send(back)
 
 
 async def search_from_saucenao(url: str) -> dict:
     api_key = config.SAUCENAO_KEY
-    proxys = config.proxies
-    # result_num = str(config.SAUCENAO_RESULT_NUM) if type(config.SAUCENAO_RESULT_NUM) == int else "5"
+    proxies = config.proxies
+    
     sau_url = "https://saucenao.com/search.php"
     async with aiohttp.ClientSession() as session:
         async with session.get(
                 sau_url + "?output_type=2&api_key=" + api_key + "&dbs[]=5&dbs[]=9&dbs[]=12&dbs[]=21&dbs[]=22&dbs[]=25&dbs[]=26&dbs[]=28&dbs[]=39&dbs[]=41&dbs[]=43&numres=1&url=" + url,
-                headers=headers, proxy=proxys.get("http")) as response:
+                headers=sauHeaders, proxy=proxies.get("http")) as response:
             return await response.json()
 
 
-async def search_from_ascii2d(url: str) -> dict:
+async def search_from_ascii2d(url: str) -> list:
+    proxies = config.proxies
+    sp = cloudscraper.create_scraper()
+    
+    _resp = sp.get("https://ascii2d.net/search/url/" + url, timeout=15, proxies=proxies)
+    _resp = await asyncio.get_running_loop().run_in_executor(None, partial(sp.get, "https://ascii2d.net/search/url/" + url, timeout=15, proxies=proxies))
+    ascUrl: str = _resp.url
+    if ascUrl.find("color") != -1:
+        await resp.finish(Message.template("{str}").format(str="ascii2d被cloudflare五秒盾单防了，你加油"))
+    ascUrl = ascUrl.replace("color", "bovw")
+    
     async with aiohttp.ClientSession() as session:
-        # 构造请求的URL
-        search_url = 'https://ascii2d.com/search/uri/{}'.format(url)
-        async with session.get(search_url) as response:
+        async with session.get(ascUrl, headers=ascHeaders, proxy=proxies.get("http")) as response:
             html = await response.text()
-            soup = BeautifulSoup(html, 'html.parser')
-
-            results = []
-            for item in soup.select('.detail-box'):
-                result = {
-                    'title': item.select_one('.detail-title').text.strip(),
-                    'url': item.select_one('.detail-link-box a')['href'],
-                    'thumbnail': item.select_one('.detail-box-img img')['src'],
-                    'similarity': item.select_one('.detail-point').text.strip()
-                }
-                results.append(result)
-            return results
+    
+    soup = BeautifulSoup(html, 'html.parser')
+    results = []
+    for item in soup.select('.item-box')[1:]:
+        
+        thumbnail = "https://ascii2d.net" + item.select('.image-box img')[0]['src']
+        datas = []
+        info = item.select_one('.info-box .detail-box')
+        if info.find('h6') is None:
+            datas.append({
+                'source': info.select_one('.external a')['href'],
+                'title': info.select_one('.external').text,
+                'author': info.select_one('.external a').text
+            })
+        else:
+            for _info in info.select('h6'):
+                datas.append({
+                    'source': _info.select('a')[0]['href'],
+                    'title': _info.select('a')[0].text,
+                    'author': _info.select('a')[1].text
+                })
+        
+        results.append({
+            'thumbnail': thumbnail,
+            'datas': datas
+        })
+    
+    return results
--- skadi/plugins/picsearch/types.py
+++ skadi/plugins/picsearch/types.py
@@ -178,7 +178,7 @@ data_models: Dict = {
     43: {'model': KemonoData, 'title': 'title', 'author': 'user_name'},
 }
 
-headers = {
+sauHeaders = {
     'Accept': 'application/json',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -186,3 +186,13 @@ headers = {
     'Connection': 'keep-alive',
     'Host': 'saucenao.com',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+
+ascHeaders = {
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+    "origin": "https://ascii2d.net",
+    "Referer": 'https://ascii2d.net/',
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "zh-CN,zh;q=0.9",
+    "dnt": "1",
+}
